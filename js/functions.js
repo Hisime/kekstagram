28079e033494c20978ef17e99c3ae0a8117b8aa7
Anna <rihisime@gmail.com>
2025-12-18 22:21:43 +0000

    Линтер и исправления

--- js/functions.js
+++ js/functions.js
@@ -1,38 +1,46 @@
-const compareStringLength = (string, length) =>  string.length <= length
+const compareStringLength = (string, length) => string.length <= length;
+
+compareStringLength('проверяемая строка', 20); // true
+compareStringLength('проверяемая строка', 18); // true
+compareStringLength('проверяемая строка', 10); // false
 
 const isPalindrome = (string) => {
-    let newString = '';
-    let normalizedString = string.toLowerCase().replaceAll(" ", "");
+  let newString = '';
+  const normalizedString = string.toLowerCase().replaceAll(' ', '');
 
-    for (let i = normalizedString.length - 1; i >= 0; i--) {
-       newString+=normalizedString[i];
-    }
+  for (let i = normalizedString.length - 1; i >= 0; i--) {
+    newString += normalizedString[i];
+  }
 
-    return normalizedString === newString ? true : false;
-}
+  return normalizedString === newString;
+};
+
+isPalindrome('топот'); // true
+isPalindrome('ДовОд'); // true
+isPalindrome('Кекс'); // false
 
 const getNumbers = (string) => {
-    let numberStr = '';
+  let numberStr = '';
 
-    let normalizedString;
+  let normalizedString;
 
-    if (typeof string === 'number') {
-        normalizedString = string.toString();
-    } else {
-        normalizedString = string.replaceAll(' ', '');
-    } 
+  if (typeof string === 'number') {
+    normalizedString = string.toString();
+  } else {
+    normalizedString = string.replaceAll(' ', '');
+  }
 
-    for (let i = 0; i < normalizedString.length; i++) {
-        if (!isNaN(normalizedString[i])) {
-            numberStr+=normalizedString[i];
-        }
+  for (let i = 0; i < normalizedString.length; i++) {
+    if (!isNaN(normalizedString[i])) {
+      numberStr += normalizedString[i];
     }
-    
-    if (numberStr === '') {
-        return 'NaN'
-    } else {
-        return parseInt(numberStr, 10);
-    }
-}
+  }
+
+  if (numberStr === '') {
+    return 'NaN';
+  }
+  return parseInt(numberStr, 10);
+};
 
 
+getNumbers('2023 год');
